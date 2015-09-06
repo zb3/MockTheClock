f08ed3cb4f56d0874bb277388b7a823da1c07c10
Michael Traw <sgv@o2.pl>
2015-09-06 14:43:29 +0200

    Format MTCWorker.js

--- js/MTCWorker.js
+++ js/MTCWorker.js
@@ -1,17 +1,15 @@
 importScripts('MTC.js')
 
-addEventListener('message', function(e)
-{
- if (typeof e.data === 'object' && e.data!==null && e.data.MTCInit)
- {
-  MTC.toggleSpoofing(e.data.spoofOn);
-  MTC.setTime(e.data.currentTime);
-  importScripts(e.data.url);
- }
- 
- if (typeof e.data === 'object' && e.data!==null && e.data.MTCcmd)
- MTC[e.data.MTCcmd](e.data.param);
+addEventListener('message', function(e) {
+  if (typeof e.data === 'object' && e.data !== null && e.data.MTCInit) {
+    MTC.toggleSpoofing(e.data.spoofOn);
+    MTC.setTime(e.data.currentTime);
+    importScripts(e.data.url);
+  }
 
- if (typeof e.data === 'object' && e.data!==null && (e.data.MTCcmd || e.data.MTCInit))
- e.stopImmediatePropagation();
+  if (typeof e.data === 'object' && e.data !== null && e.data.MTCcmd)
+    MTC[e.data.MTCcmd](e.data.param);
+
+  if (typeof e.data === 'object' && e.data !== null && (e.data.MTCcmd || e.data.MTCInit))
+    e.stopImmediatePropagation();
 }, true);
